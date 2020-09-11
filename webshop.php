991ac8bdb8aa96264919690a2670fc29378b03b5
asphalt13 <71091456+asphalt13@users.noreply.github.com>
2020-09-11 09:56:52 +0200

    Update webshop.php
    change title

--- webshop.php
+++ webshop.php
@@ -8,7 +8,7 @@ $com = new com;
 $search_field_id = "search_field_1";
 $table_id = "table_1";
 
-$view->print_html_page_start("Web store", true, $search_field_id, $table_id);
+$view->print_html_page_start("Web shop", true, $search_field_id, $table_id);
 $view->print_html_webshop_table($search_field_id, $table_id);
 $view->print_html_page_end();
 
